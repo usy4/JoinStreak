81b5b5bed496346383d5a90b9db17a6ba50f06cc
usy4 <68358524+usy4@users.noreply.github.com>
2023-01-25 16:09:39 +0300

    Update EventListener.php

--- src/usy4/JoinStreak/EventListener.php
+++ src/usy4/JoinStreak/EventListener.php
@@ -23,11 +23,11 @@ class EventListener implements Listener {
             if($time_diff <= 86400){  // 86400 seconds in a day :O
                 $streak++;
                 $config->set($name, $streak);
-                $player->sendMessage("Welcome back! Your join streak is now $streak days.");
+                $player->sendMessage("§7Welcome back! Your join streak is now §c$streak §7days.");
             } else {
                 $streak = 1;
                 $config->set($name, $streak);
-                $player->sendMessage("Welcome back! Your join streak has reset to 1 day.");
+                $player->sendMessage("Welcome back! Your join streak has reset to §c1 §7day.");
             }
             $config->set($name . "_last_join", $now);
             $config->save();
@@ -35,7 +35,7 @@ class EventListener implements Listener {
             $config->set($name, 1);
             $config->set($name . "_last_join", time());
             $config->save();
-            $player->sendMessage("Welcome to the server! This is your first time playing, so your join streak is 1 day.");
+            $player->sendMessage("§7Welcome to the server! This is your first time playing, so your join streak is §c1 §7day.");
         }
     }
 
