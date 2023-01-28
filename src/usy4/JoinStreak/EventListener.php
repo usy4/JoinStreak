abcc2fdb4a535f09005867f78a247e7270d6c7a8
usy4 <68358524+usy4@users.noreply.github.com>
2023-01-28 16:25:09 +0300

    Update EventListener.php

--- src/usy4/JoinStreak/EventListener.php
+++ src/usy4/JoinStreak/EventListener.php
@@ -20,18 +20,25 @@ class EventListener implements Listener {
         if($config->exists($name)){
             $streak = $config->get($name);
             $last_join_date = $config->get($name . "_last_join_date");
-            if ($last_join_date == $now_date) {
+            $last_join_time = $config->get($name . "_last_join_time");
+            $time_diff = $now - $last_join_time;
+            if ($last_join_date == $now_date && $time_diff <= 300) {
                 $player->sendMessage("§7Welcome back! Your join streak remains at§c" . $streak . "§7days.");
+            } elseif ($last_join_date == $now_date && $time_diff > 300) {
+                $config->set($name, 1);
+                $player->sendMessage("§7Welcome back! But your join streak has been reset to " . "§c1" . "§7 because you did not join within the given time.");
             } else {
                 $streak++;
                 $config->set($name, $streak);
                 $player->sendMessage("§7Welcome back! Your join streak is now§c" . $streak . "§7days.");
             }
             $config->set($name . "_last_join_date", $now_date);
+            $config->set($name . "_last_join_time", $now);
             $config->save();
         } else {
             $config->set($name, 1);
             $config->set($name . "_last_join_date", date('Y-m-d', $now));
+            $config->set($name . "_last_join_time", $now);
             $config->save();
             $player->sendMessage("§7Welcome to the server! This is your first time playing, so your join streak is§c" . "1" . "§7day.");
         }
